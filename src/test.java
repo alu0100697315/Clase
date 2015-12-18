a1da71c94330fd55462b0737fe1a777c7c1d6aef
alu0100697315 <alu0100697315@ull.edu.es>
2015-12-18 14:02:30 +0000

    otro test

--- src/test.java
+++ src/test.java
@@ -3,14 +3,18 @@ import static org.junit.Assert.*;
 import knapsack.problem.TesterAlgorithmPortfolio;
 
 public class Test {
-
+	
+	@org.junit.Test
+	public void test() {
+		fail("Not yet implemented");
+	}
 	@org.junit.Test(expected = IllegalArgumentException.class)
 	  public void Test() {
-		Test tester = new Test();
+		Test prueba = new Test();
 	  }
 	
 	@org.junit.Test(expected = IllegalArgumentException.class)
-	  public void TesterAlgorithmPortfolio() {
+	 public void TesterAlgorithmPortfolio() {
 		TesterAlgorithmPortfolio tester = new TesterAlgorithmPortfolio();
 	  }
 }
